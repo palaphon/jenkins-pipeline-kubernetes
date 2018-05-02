89d01deb9e3fce9fc12e1c9f48c82497bd9024b8
Palaphon Chunpong <34329315+palaphon@users.noreply.github.com>
2018-05-02 20:26:17 +0700

    comment curl upload helm

--- build.sh
+++ build.sh
@@ -113,7 +113,7 @@ pushHelmChart() {
 	helm version --tls
 	
     [ ! -z "${chart_name}" ] || errorExit "Did not find the helm chart to deploy"
-    //curl -u${HELM_USR}:${HELM_PSW} -T ${chart_name} "${HELM_REPO}/$(basename ${chart_name})" || errorExit "Uploading helm chart failed"
+    #curl -u${HELM_USR}:${HELM_PSW} -T ${chart_name} "${HELM_REPO}/$(basename ${chart_name})" || errorExit "Uploading helm chart failed"
 	helm lint --strict ${chart_name} || errorExit "helm lint --strict chart failed"
 	helm package demoapp ${chart_name} || errorExit "Uploading helm chart failed"
 	bx pr load-helm-chart --archive ${BUILD_DIR}/helm/*.tgz 
